d4186ec412f8913c16bde55b9de8a34094f6384d
koredeoki <oluwakoredeoki@gmail.com>
2025-12-13 16:23:27 -0600

    Add files via upload

---
+++ translate.py
@@ -0,0 +1,16 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> sentence = input("Enter a text: ")
+Enter a text: I love custard and cake
+>>> sentence1 = sentence.replace("a", "4")
+>>> sentence2 = sentence1.replace("b", "8")
+>>> sentence3 = sentence2.replace("e", "3")
+>>> sentence4 = sentence3.replace("1", "1")
+>>> sentence5 = sentence4.replace("o", "0")
+>>> sentence6 = sentence5.replace("s", "5")
+>>> sentence7 = sentence6.replace("t", "7")
+>>> print(sentence7)
+I l0v3 cu574rd 4nd c4k3
+>>> final_sentence = sentence7
+>>> print(final_sentence)
+I l0v3 cu574rd 4nd c4k3
